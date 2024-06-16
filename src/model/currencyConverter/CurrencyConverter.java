37b997fdc0c19a465deba6fc370aa809ca267a4d
JONATHAN <kyeyunejonathan001@gmail.com>
2024-06-16 14:56:56 +0300

    commit

--- src/model/currencyConverter/CurrencyConverter.java
+++ src/model/currencyConverter/CurrencyConverter.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
 	
-	
+	static Scanner scan = new Scanner(System.in);
 	
 	public static void dispalyMenu() {
+		System.out.println();
+		System.out.println();
 		System.out.println("WELCOME TO JONATHAN CURRENCY CONVERTER");
 		System.out.println();
 		System.out.println("1	:	(EUR) EURO");
@@ -22,10 +24,10 @@ public class CurrencyConverter {
 	}
 	
 	public static void start() {
+		
 		while(true) {
 		dispalyMenu();
 		
-		Scanner scan = new Scanner(System.in);
 		System.out.print("SELECT CURRENCY : 	");
 		int option = scan.nextInt();
 		
@@ -33,39 +35,68 @@ public class CurrencyConverter {
 		 case 1 :
 			 System.out.println();
 			 System.out.print("ENTER AMOUNT		");
-			 double euro = scan.nextInt();
+			 double euro = scan.nextDouble();
 			 System.out.println();
 			 convertEuroAmount(euro);
 			 break;
 		 case 2 :
 			 System.out.println();
 			 System.out.print("ENTER AMOUNT		");
-			 double pound = scan.nextInt();
+			 double pound = scan.nextDouble();
 			 System.out.println();
 			 convertPoundAmount(pound);
 			 break;
 		 case 3 :
-			 
+			 System.out.println();
+			 System.out.print("ENTER AMOUNT		");
+			 double USD = scan.nextDouble();
+			 System.out.println();
+			 convertUsdAmount(USD);
 			 break;
 		 case 4 :
-			 
+			 System.out.println();
+			 System.out.print("ENTER AMOUNT		");
+			 double rupee = scan.nextDouble();
+			 System.out.println();
+			 convertRupeeAmount(rupee);
 			 break;
 		 case 5 :
-			 
+			 System.out.println();
+			 System.out.print("ENTER AMOUNT		");
+			 double cad = scan.nextDouble();
+			 System.out.println();
+			 convertCadAmount(cad);
 			 break;
 		 case 6 :
-			 
+			 System.out.println();
+			 System.out.print("ENTER AMOUNT		");
+			 double aud = scan.nextDouble();
+			 System.out.println();
+			 convertAudAmount(aud);
 			 break;
 		 case 7 :
-			 
+			 System.out.println();
+			 System.out.print("ENTER AMOUNT		");
+			 double SGD = scan.nextDouble();
+			 System.out.println();
+			 convertSgdAmount(SGD);
 			 break;
-			 
+		 
+		 case 8 :
+			 System.out.println("THANK YOU FOR TRUSTING OUR SERVICES");
+			 System.out.println("...........SYSTEM CLOSE............");
+			 System.out.println("@Copyright Reserved Jonathan-Tech.");
+			 System.exit(0);
+			 break;
 		 }
+		 
 		}
 	}
 
+	
+
 	private static void convertEuroAmount(double amount) {
-		Scanner scan = new Scanner(System.in);
+		
 			optionMenu(amount);
 		System.out.print("ENTER OPTION  :		");
 		int opt = scan.nextInt();
@@ -110,10 +141,11 @@ public class CurrencyConverter {
 			System.out.println();
 			System.out.println(amount +" EUR  =  "+ newAmount +" SGD");	
 		}
+		
 	}
 	//
 	private static void convertPoundAmount(double amount) {
-		Scanner scan = new Scanner(System.in);
+	
 			optionMenu(amount);
 		System.out.print("ENTER OPTION  :		");
 		int opt = scan.nextInt();
@@ -159,8 +191,267 @@ public class CurrencyConverter {
 			System.out.println();
 			System.out.println(amount +" GBP  =  "+ newAmount +" SGD");	
 		}
+		
 	}
 	
+	//
+	private static void convertUsdAmount(double amount) {
+		
+			optionMenu(amount);
+		System.out.print("ENTER OPTION  :		");
+		int opt = scan.nextInt();
+		
+		if(opt == 1 ) {
+			System.out.println("1  USD  =  0.93 EUR");
+			double newAmount = amount * 0.93;
+			System.out.println();
+			System.out.println(amount +" USD  =  "+ newAmount +" EUR");
+			
+		}
+		else if(opt == 2 ) {
+			System.out.println("1  USD   =  0.79 GBP");
+			double newAmount = amount * 0.79;
+			System.out.println();
+			System.out.println(amount +" USD   =  "+ newAmount +" GBP");
+			
+		}
+		else if(opt == 3 ) {
+			System.out.println();
+			System.out.println(amount +" USD  =  "+ amount + " USD ");
+		}
+		else if(opt == 4 ) {
+			System.out.println("1  USD   =  83.55 INR");
+			double newAmount = amount * 83.55;
+			System.out.println();
+			System.out.println(amount +" USD   =  "+ newAmount +" INR");
+		}
+		else if(opt == 5 ) {
+			System.out.println("1  USD   =  1.37 CAD");
+			double newAmount = amount * 1.37;
+			System.out.println();
+			System.out.println(amount +" USD   =  "+ newAmount +" CAD");	
+		}
+		else if(opt == 6) {
+			System.out.println("1  USD   =  1.57 AUD");
+			double newAmount = amount * 1.57;
+			System.out.println();
+			System.out.println(amount +" USD   =  "+ newAmount +" AUD");
+		}
+		else if(opt == 7) {
+			System.out.println("1  USD   =  1.35 SGD");
+			double newAmount = amount * 1.35;
+			System.out.println();
+			System.out.println(amount +" USD   =  "+ newAmount +" SGD");	
+		}
+		
+	}
+	
+	//
+	private static void convertRupeeAmount(double amount) {
+		
+			optionMenu(amount);
+		System.out.print("ENTER OPTION  :		");
+		int opt = scan.nextInt();
+		
+		if(opt == 1 ) {
+			System.out.println("1  INR  =  0.01 EUR");
+			double newAmount = amount * 0.01;
+			System.out.println();
+			System.out.println(amount +" INR  =  "+ newAmount +" EUR");
+			
+		}
+		else if(opt == 2 ) {
+			System.out.println("1  INR   =  0.01 GBP");
+			double newAmount = amount * 0.01;
+			System.out.println();
+			System.out.println(amount +" INR   =  "+ newAmount +" GBP");
+			
+		}
+		else if(opt == 3 ) {
+			System.out.println("1  INR   =  0.01 USD");
+			double newAmount = amount * 0.01;
+			System.out.println();
+			System.out.println(amount +" INR   =  "+ newAmount +" USD");
+		}
+		else if(opt == 4 ) {
+			System.out.println();
+			System.out.println(amount +" INR  =  "+ amount + " INR ");
+		}
+		else if(opt == 5 ) {
+			System.out.println("1  INR   =  0.02 CAD");
+			double newAmount = amount * 0.02;
+			System.out.println();
+			System.out.println(amount +" INR   =  "+ newAmount +" CAD");	
+		}
+		else if(opt == 6) {
+			System.out.println("1  INR   =  0.02 AUD");
+			double newAmount = amount * 0.02;
+			System.out.println();
+			System.out.println(amount +" INR   =  "+ newAmount +" AUD");
+		}
+		else if(opt == 7) {
+			System.out.println("1  INR   =  0.02 SGD");
+			double newAmount = amount * 0.02;
+			System.out.println();
+			System.out.println(amount +" INR   =  "+ newAmount +" SGD");	
+		}
+		
+	}
+	//
+	private static void convertCadAmount(double amount) {
+		
+		optionMenu(amount);
+	System.out.print("ENTER OPTION  :		");
+	int opt = scan.nextInt();
+	
+	if(opt == 1 ) {
+		System.out.println("1  CAD  =  0.68 EUR");
+		double newAmount = amount * 0.68;
+		System.out.println();
+		System.out.println(amount +" CAD  =  "+ newAmount +" EUR");
+		
+	}
+	else if(opt == 2 ) {
+		System.out.println("1  CAD   =  0.57 GBP");
+		double newAmount = amount * 0.57;
+		System.out.println();
+		System.out.println(amount +" CAD   =  "+ newAmount +" GBP");
+		
+	}
+	else if(opt == 3 ) {
+		System.out.println("1  CAD   =  0.73 USD");
+		double newAmount = amount * 0.73;
+		System.out.println();
+		System.out.println(amount +" CAD   =  "+ newAmount +" USD");
+	}
+	else if(opt == 4 ) {
+		System.out.println("1  CAD   =  60.83 INR");
+		double newAmount = amount * 60.83;
+		System.out.println();
+		System.out.println(amount +" CAD   =  "+ newAmount +" INR");
+	}
+	else if(opt == 5 ) {
+		System.out.println();
+		System.out.println(amount +" CAD  =  "+ amount + " CAD ");	
+	}
+	else if(opt == 6) {
+		System.out.println("1  CAD   =  1.10 AUD");
+		double newAmount = amount * 1.10;
+		System.out.println();
+		System.out.println(amount +" CAD   =  "+ newAmount +" AUD");
+	}
+	else if(opt == 7) {
+		System.out.println("1  CAD   =  0.99 SGD");
+		double newAmount = amount * 0.99;
+		System.out.println();
+		System.out.println(amount +" CAD   =  "+ newAmount +" SGD");	
+	}
+	
+	}
+	//
+	private static void convertAudAmount(double amount) {
+		
+		optionMenu(amount);
+	System.out.print("ENTER OPTION  :		");
+	int opt = scan.nextInt();
+	
+	if(opt == 1 ) {
+		System.out.println("1  AUD  =  0.62 EUR");
+		double newAmount = amount * 0.62;
+		System.out.println();
+		System.out.println(amount +" AUD  =  "+ newAmount +" EUR");
+		
+	}
+	else if(opt == 2 ) {
+		System.out.println("1  AUD   =  0.52 GBP");
+		double newAmount = amount * 0.52;
+		System.out.println();
+		System.out.println(amount +" AUD   =  "+ newAmount +" GBP");
+		
+	}
+	else if(opt == 3 ) {
+		System.out.println("1  AUD   =  0.66 USD");
+		double newAmount = amount * 0.66;
+		System.out.println();
+		System.out.println(amount +" AUD   =  "+ newAmount +" USD");
+	}
+	else if(opt == 4 ) {
+		System.out.println("1  AUD   =  55.27 INR");
+		double newAmount = amount * 55.27;
+		System.out.println();
+		System.out.println(amount +" AUD  =  "+ newAmount +" INR");
+	}
+	else if(opt == 5 ) {
+		System.out.println("1  AUD   =  0.91 CAD");
+		double newAmount = amount * 0.91;
+		System.out.println();
+		System.out.println(amount +" AUD   =  "+ newAmount +" CAD");
+	}
+	else if(opt == 6) {
+		System.out.println();
+		System.out.println(amount +" AUD  =  "+ amount + " AUD ");
+	}
+	else if(opt == 7) {
+		System.out.println("1  AUD   =  0.90 SGD");
+		double newAmount = amount * 0.90;
+		System.out.println();
+		System.out.println(amount +" AUD   =  "+ newAmount +" SGD");	
+	}
+	
+	}
+	
+	//
+	private static void convertSgdAmount(double amount) {
+		
+		optionMenu(amount);
+	System.out.print("ENTER OPTION  :		");
+	int opt = scan.nextInt();
+	
+	if(opt == 1 ) {
+		System.out.println("1  SGD  =  0.69 EUR");
+		double newAmount = amount * 0.69;
+		System.out.println();
+		System.out.println(amount +" SGD  =  "+ newAmount +" EUR");
+		
+	}
+	else if(opt == 2 ) {
+		System.out.println("1  SGD   =  0.58 GBP");
+		double newAmount = amount * 0.58;
+		System.out.println();
+		System.out.println(amount +" SGD   =  "+ newAmount +" GBP");
+		
+	}
+	else if(opt == 3 ) {
+		System.out.println("1  SGD   =  0.74 USD");
+		double newAmount = amount * 0.74;
+		System.out.println();
+		System.out.println(amount +" SGD   =  "+ newAmount +" USD");
+	}
+	else if(opt == 4 ) {
+		System.out.println("1  SGD   =  61.73 INR");
+		double newAmount = amount * 61.73;
+		System.out.println();
+		System.out.println(amount +" SGD  =  "+ newAmount +" INR");
+	}
+	else if(opt == 5 ) {
+		System.out.println("1  SGD   =  1.01 CAD");
+		double newAmount = amount * 1.01;
+		System.out.println();
+		System.out.println(amount +" SGD   =  "+ newAmount +" CAD");
+	}
+	else if(opt == 6) {
+		System.out.println("1  SGD   =  1.12 AUD");
+		double newAmount = amount * 1.12;
+		System.out.println();
+		System.out.println(amount +" SGD   =  "+ newAmount +" AUD");
+	}
+	else if(opt == 7) {
+		System.out.println();
+		System.out.println(amount +" SGD  =  "+ amount + " SGD ");
+	}
+	
+	}
+	//
 	public static void optionMenu(double amount) {
 		System.out.println("  CONVERT  "+amount + "  TO  ");
 		System.out.println();
